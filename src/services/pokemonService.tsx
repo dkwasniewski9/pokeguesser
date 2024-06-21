8d53b5a5327967c105742baeb429005132cae460
Dawid Kwaśniewski <58298740+dkwasniewski9@users.noreply.github.com>
2024-06-21 13:45:30 +0200

    Fixing some names received from fetch

--- src/services/pokemonService.tsx
+++ src/services/pokemonService.tsx
@@ -5,7 +5,7 @@ export const getRandomPokemon = async(): Promise<Pokemon> => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${dexNumber}`)
         .then((response) => response.json())
     return {
-        name: response.name,
+        name: response.name.split('-')[0],
         url: response.sprites.other['official-artwork'].front_default
     }
 }
