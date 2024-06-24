1644e20421a72188fe60e37ffb1d8e4235d25f7e
Dawid Kwaśniewski <58298740+dkwasniewski9@users.noreply.github.com>
2024-06-24 23:12:49 +0200

    Fix typing

--- src/utils/getHint.tsx
+++ src/utils/getHint.tsx
@@ -5,16 +5,15 @@ export function getHint(guess: string | number | string[], correct: string | num
     if(typeof guess === 'string'){
             return 'wrong'
     }
-    else if(typeof guess === 'number'){
-        const guessNumber:number = Number(guess)
-        if(guessNumber < correct){
+    else if(typeof guess === 'number' && typeof correct === 'number'){
+        if(Number(guess) < correct){
             return 'higher'
         }
         else{
             return 'lower'
         }
     }
-    else {
+    else if(Array.isArray(guess) && Array.isArray(correct)){
         if(guess.some(type  => (correct as string[]).includes(type))){
             return 'partial'
         }
