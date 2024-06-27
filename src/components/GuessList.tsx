fe553c963488f6a56f6094a77ad48f9d5b684bac
Dawid Kwaśniewski <58298740+dkwasniewski9@users.noreply.github.com>
2024-06-27 22:16:48 +0200

    Fix css for mobile

--- src/components/GuessList.tsx
+++ src/components/GuessList.tsx
@@ -28,36 +28,37 @@ const GuessList: React.FC<GuessListProps> = (props: GuessListProps) => {
                 {props.guesses.length > 0 ? (
                     props.guesses.slice().reverse().map((guess, index) => (
                         <tr key={index}>
-                            <td className={guess.hints.dexNumber}>
+                            <td data-label="Dex Number" className={guess.hints.dexNumber}>
                                 {guess.pokemon.dexNumber}
                                 {guess.hints.dexNumber === 'lower' ? (<p className="extra-text">(Lower)</p>) : ('')}
                                 {guess.hints.dexNumber === 'higher' ? (<p className="extra-text">(Higher)</p>) : ('')}
                             </td>
-                            <td className={guess.hints.name}><img src={guess.pokemon.url} alt={guess.pokemon.name}
-                                                                  style={{maxWidth: '100px'}}/></td>
-                            <td className={guess.hints.name}>{guess.pokemon.name}</td>
-                            <td className={guess.hints.height}>
+                            <td data-label="Image" className={guess.hints.name}>
+                                <img src={guess.pokemon.url} alt={guess.pokemon.name} style={{maxWidth: '100px'}}/>
+                            </td>
+                            <td data-label="Name" className={guess.hints.name}>{guess.pokemon.name}</td>
+                            <td data-label="Height" className={guess.hints.height}>
                                 {guess.pokemon.height}
                                 {guess.hints.height === 'lower' ? (<p className="extra-text">(Lower)</p>) : ('')}
                                 {guess.hints.height === 'higher' ? (<p className="extra-text">(Higher)</p>) : ('')}
                             </td>
-                            <td className={guess.hints.weight}>
+                            <td data-label="Weight" className={guess.hints.weight}>
                                 {guess.pokemon.weight}
                                 {guess.hints.weight === 'lower' ? (<p className="extra-text">(Lower)</p>) : ('')}
                                 {guess.hints.weight === 'higher' ? (<p className="extra-text">(Higher)</p>) : ('')}
                             </td>
-                            <td className={guess.hints.types}>{guess.pokemon.types.join(', ')}</td>
-                            <td className={guess.hints.generation}>
+                            <td data-label="Types" className={guess.hints.types}>{guess.pokemon.types.join(', ')}</td>
+                            <td data-label="Generation" className={guess.hints.generation}>
                                 {guess.pokemon.generation}
                                 {guess.hints.generation === 'lower' ? (<p className="extra-text">(Lower)</p>) : ('')}
                                 {guess.hints.generation === 'higher' ? (<p className="extra-text">(Higher)</p>) : ('')}
                             </td>
-                            <td className={guess.hints.color}>{guess.pokemon.color}</td>
+                            <td data-label="Color" className={guess.hints.color}>{guess.pokemon.color}</td>
                         </tr>
                     ))
                 ) : (
                     <tr>
-                        <td colSpan={8}><p>No guessed pokemon</p></td>
+                        <td colSpan={8}><p className={'no-guess'}>No guessed pokemon</p></td>
                     </tr>
                 )
                 }
